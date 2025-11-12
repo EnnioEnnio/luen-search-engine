6b9e99d0cebb2e967c5a05b1f252e596ffbe6a08
Ennio Strohauer <ennio.strohauer@gmail.com>
2025-11-12 20:10:03 +0100

    type cleanup for later reuse

--- internal/model/types.go
+++ internal/model/types.go
@@ -1,4 +1,21 @@
 package model
 
+// DocID is the first column of the dataset
 type DocID = uint32
+
+// Token is a word, either in a query, or in a document text corpus (title/body)
 type Token = string
+
+// Match keeps track of how often a token occurred in a document.
+type Match struct {
+	Token     Token
+	Frequency int
+	Positions []int
+}
+
+// Result represents the aggregated scores for a matching document.
+type Result struct {
+	DocID              DocID
+	TotalTermFrequency int // used for ranking only
+	Matches            []Match
+}
